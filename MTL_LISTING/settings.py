3e4bf4f7ed581c5ea6b22aaa1eb8b2ca68b2fb65
ORC-1 <Rolandazim@gmail.com>
2018-11-05 11:14:50 +0100

    Beta

--- MTL_LISTING/settings.py
+++ MTL_LISTING/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k0aax!b_2zb9-cwet@skb2i^j%p0t-_yligud50qdka_(yp+3i'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,10 +141,9 @@ REST_FRAMEWORK = {
 # }
 
 # AWS settings
-
-AWS_ACCESS_KEY_ID = "AKIAI7PCEA64GBUASIXA"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_STORAGE_BUCKET_NAME = "mtl-listing"
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
